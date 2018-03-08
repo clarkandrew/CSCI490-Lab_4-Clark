a9b6ca42b2211e605629f5c40066fd32132bf80b
BuildTools <unconfigured@null.spigotmc.org>
2018-03-08 18:41:31 -0500

    url connection lab

--- app/src/main/java/com/example/andrewclark/csci490_lab_04_clark/MainActivity.java
+++ app/src/main/java/com/example/andrewclark/csci490_lab_04_clark/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.andrewclark.csci490_lab_04_clark;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            String output = sendGet("https://jsonplaceholder.typicode.com/users");
+            Log.i("Output: ", output);
+
+        } catch(Exception e) {
+            Log.i("Error: ", e.getMessage());
+        }
+
+    }
+
+
+
+    public String sendGet(String iUrl)  {
+        String output;
+        URL getUrl;
+
+
+                getUrl = new URL(iUrl);
+
+                HttpURLConnection conn = (HttpURLConnection) getUrl.openConnection();
+
+                InputStreamReader streamRead = new InputStreamReader((conn.getInputStream()));
+
+                BufferedReader buffRead = new BufferedReader(streamRead);
+
+                output = buffRead.readLine();
+
+                buffRead.close();
+
+
+        return output;
     }
 }
